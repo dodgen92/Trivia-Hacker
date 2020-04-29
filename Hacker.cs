a58a5b6cb5eca1823c90694bbe34e0ed176aaa41
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-04-29 16:35:22 -0400

    added message input

---
+++ Hacker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        ShowMainMenu("Greetings Hacker");
+    }
+    
+    void ShowMainMenu (){
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Select who's computer to hack");
+        Terminal.WriteLine("1. Sports Journalist");
+        Terminal.WriteLine("2. Movie Critic");
+        Terminal.WriteLine("3. NASA Engineer");
+        Terminal.WriteLine("Enter your selection");
+    }
+
+    void OnUserInput(string input)
+    {
+        if(input == 1){
+            print("level 1")
+        }
+    }
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
